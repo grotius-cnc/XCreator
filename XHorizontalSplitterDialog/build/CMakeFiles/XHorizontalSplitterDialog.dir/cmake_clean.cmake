4196fc791f4814818056d3e4944380123a66aaa6
grotius-cnc <michelwijnja@gmail.com>
2022-10-07 09:24:39 -0400

    commit

---
+++ XHorizontalSplitterDialog/build/CMakeFiles/XHorizontalSplitterDialog.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/XHorizontalSplitterDialog.dir/main.cpp.o"
+  "CMakeFiles/XHorizontalSplitterDialog.dir/main.cpp.o.d"
+  "XHorizontalSplitterDialog"
+  "XHorizontalSplitterDialog.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/XHorizontalSplitterDialog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
