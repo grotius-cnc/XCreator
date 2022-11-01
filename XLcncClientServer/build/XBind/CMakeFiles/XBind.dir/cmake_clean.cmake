b85bea5a73de6416474e4deffa7881c92889d99d
User <user@debian-BULLSEYE-live-builder-AMD64>
2022-11-01 10:32:23 +0100

    commit

---
+++ XLcncClientServer/build/XBind/CMakeFiles/XBind.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/XBind.dir/XBind.cpp.o"
+  "CMakeFiles/XBind.dir/XClient.cpp.o"
+  "CMakeFiles/XBind.dir/lodepng/lodepng.cpp.o"
+  "libXBind.pdb"
+  "libXBind.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/XBind.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ XLcncClientServer/build/XServer/CMakeFiles/XServer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/XServer.dir/home/user/XCreator/vendor/lodepng/lodepng.cpp.o"
+  "CMakeFiles/XServer.dir/main.cpp.o"
+  "XServer"
+  "XServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/XServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
